5958ab1ca3ebdff324dd8ad3d0d78ab6d027fc35
Amaccarrone <antonio.m.maccarrone@gmail.com>
2018-09-19 15:20:57 +0200

    def Test location
    Se movio la función debajo para mayor visibilidad y se sacaron algunos time.sleep que desperdiciaban tiempo

--- instaUsers.py
+++ instaUsers.py
@@ -16,26 +16,26 @@ class toni(unittest.TestCase):
         self.driver = webdriver.Chrome('chromedriver.exe')
         self.driver.maximize_window()
         self.driver.get("https://www.instagram.com/accounts/login/?hl=es")
-        time.sleep(1)
+        self.driver.implicitly_wait(5)
 
-#self.user.send_keys('plus54vpn@gmail.com')
+#self.user.send_keys('username')
         self.user = self.driver.find_element_by_name('username')
-        self.user.send_keys('tonymaccarroneph')
-#self.password.send_keys('tecateca12')
+        self.user.send_keys('tonymaccarrone')
+
+#self.password.send_keys('password')
         self.password = self.driver.find_element_by_name('password')
         self.password.send_keys('antonio22')
 
         self.password.send_keys(Keys.ENTER)
 
         profile: str = input('Tipear Profile: ')
-        time.sleep(1)
+        self.driver.implicitly_wait(5)
         self.driver.get('https://www.instagram.com/'+profile+'/?hl=es')
-        time.sleep(2)
+        self.driver.implicitly_wait(5)
         continuar = str
         while continuar != 'si':
             continuar = input('Arrancamos?')
             time.sleep(0.5)
-#self.driver.find_element_by_class_name('_9AhH0').click()
 
     def detectVideo(self):
         try:
@@ -43,32 +43,30 @@ class toni(unittest.TestCase):
             a = True
         except NoSuchElementException:
             a = False
-
         return a
 
     def findUserThatLikes(self):
-        time.sleep(1)
         while self.detectVideo() == False:
             try:
                 self.driver.find_element_by_tag_name('body').send_keys(Keys.ARROW_RIGHT)
-                time.sleep(2)
+                self.driver.implicitly_wait(5)
                 self.detectVideo()
             except NoSuchElementException:
                 pass
         userThatLikes = self.driver.find_element_by_class_name('zV_Nj')
         qty = userThatLikes.text
         print('\n')
-        print (qty, 'sin modificar')
+        print(qty, 'sin modificar')
         userThatLikes.click()
-        time.sleep(2)
+        self.driver.implicitly_wait(5)
         qty = re.sub("[^0-9]", "", qty)
-        qty= int(qty)
+        qty = int(qty)
         rango = int(qty/10)
         if qty > 10:
-            for x in range(0,rango*3):
+            for x in range(0, rango*3):
                 xpath = 'wFPL8 '
                 scroll = self.driver.find_elements_by_class_name(xpath)
-                time.sleep(2)
+                self.driver.implicitly_wait(5)
 
                 try:
                     self.driver.execute_script("return arguments[0].scrollIntoView(true);", scroll[-1])
@@ -76,30 +74,13 @@ class toni(unittest.TestCase):
                     pass
         else:
             pass
-        time.sleep(3)
+        self.driver.implicitly_wait(5)
         self.users = self.driver.find_elements_by_class_name('FPmhX')
         print(len(self.users))
         print('\n')
-        time.sleep(2)
+        self.driver.implicitly_wait(5)
 
         return self.users
-
-    def test(self):
-        for x in range(0, 5):
-            self.findUserThatLikes()
-            self.navigateAndLike()
-        again = input('Seguimos likeando? (y/n): ')
-        if again == 'y':
-            profile = input('Tipear Profile: ')
-            self.driver.get('https://www.instagram.com/' + profile + '/?hl=es')
-            time.sleep(3)
-            continuar = str
-            while continuar != 'si':
-                continuar = input('Arrancamos?')
-                time.sleep(0.5)
-            self.test()
-        else:
-            pass
 
     def LikeForUsers(self):
         tiempo = randint(1, 4)
@@ -112,7 +93,7 @@ class toni(unittest.TestCase):
             self.driver.find_element_by_xpath("//span[@aria-label='Like']").click()
         except NoSuchElementException:
             pass
-        time.sleep(tiempo * 1.09)
+        self.driver.implicitly_wait(5)
         slidearRandom = randint(1, 4)
         for x in range(0, slidearRandom):
             self.driver.find_element_by_tag_name('body').send_keys(Keys.ARROW_RIGHT)
@@ -123,7 +104,7 @@ class toni(unittest.TestCase):
             link = user.get_attribute('href')
             user.send_keys(Keys.CONTROL + Keys.ENTER)
             while (len(self.driver.window_handles)) == 1:
-                time.sleep(0.5)
+                self.driver.implicitly_wait(5)
 
             self.driver.switch_to.window(self.driver.window_handles[1])
 
@@ -135,18 +116,18 @@ class toni(unittest.TestCase):
             implicitwait = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "_mainc")))
             '''
 
-            if 'This Account is Private' in self.driver.page_source or 'No posts yet.' in self.driver.page_source or 'Esta cuenta es privada' in self.driver.page_source or 'Aún no hay publicaciones.' in self.driver.page_source or "Sorry, this page isn't available." in self.driver.page_source or "Esta página no está disponible." in self.driver.page_source or 'Oops, an error occurred' in self.driver.page_source or 'Sorry, something went wrong' in self.driver.page_source:
-                time.sleep(1.5)
+            if 'This Account is Private' in self.driver.page_source or 'No Posts Yet' in self.driver.page_source or 'Esta cuenta es privada' in self.driver.page_source or 'Aún no hay publicaciones' in self.driver.page_source or "Sorry, this page isn't available" in self.driver.page_source or "Esta página no está disponible." in self.driver.page_source or 'Oops, an error occurred' in self.driver.page_source or 'Sorry, something went wrong' in self.driver.page_source:
+                time.sleep(1)
                 pass
 
             elif self.driver.current_url in return_excelValue('Lista'):
-                print('Ya esta en la lista')
+                print(return_excelValue,'Ya esta en la lista')
 
             elif self.driver.current_url in return_excelValue('Cruda'):
                 print('Ya esta en la lista')
 
             else:
-                time.sleep(1.5)
+                time.sleep(1)
 
                 try:
                     element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "_9AhH0")))
@@ -157,7 +138,7 @@ class toni(unittest.TestCase):
                     self.driver.find_element_by_class_name('_9AhH0').click()
                 except NoSuchElementException:
                     pass
-                time.sleep(1)
+                self.driver.implicitly_wait(5)
                 for x in range(0, 5):
                     self.LikeForUsers()
                 appendLink(link)
@@ -168,14 +149,13 @@ class toni(unittest.TestCase):
         self.driver.find_element_by_tag_name('body').send_keys(Keys.ARROW_RIGHT)
 
     def LikeSlide(self):
-        tiempo = randint(5,10)
+        tiempo = randint(1, 6)
 
         try:
             self.driver.find_element_by_link_text('Me gusta').click()
         except NoSuchElementException:
             print('ya le di like')
             pass
-
 
         '''
         try:
@@ -184,6 +164,25 @@ class toni(unittest.TestCase):
             print('ya le di like')
             pass
         '''
-        time.sleep(tiempo*1.09)
+        time.sleep(tiempo*1.03)
         self.driver.find_element_by_tag_name('body').send_keys(Keys.ARROW_RIGHT)
         time.sleep(tiempo*0.75)
+
+
+    def test(self):
+        for x in range(0, 4):
+            self.findUserThatLikes()
+            self.navigateAndLike()
+        again = input('Seguimos likeando? (y/n): ')
+        if again == 'y':
+            profile = input('Tipear Profile: ')
+            self.driver.get('https://www.instagram.com/' + profile + '/?hl=es')
+            self.driver.implicitly_wait(5)
+            continuar = str
+            while continuar != 'si':
+                continuar = input('Arrancamos?')
+                self.driver.implicitly_wait(5)
+            self.test()
+        else:
+            print('Ahhh bueno terminamos entonces!')
+            pass
